2fb1d12fd4dfe84a5833112d6a731c836e3f4dc3
Ian Minara <minashcaster@gmvsdvdsvail.com>
2018-05-03 01:34:47 +0300

    Fix heroku error

--- main/watermarker.py
+++ main/watermarker.py
@@ -13,5 +13,5 @@ def watermarker(url, text, font, size):
         w, h = font.getsize(text)
         writer.text(((width-w)/2, (height-h)/2), text, font=font)
     output_gif = frames[0]
-    output_gif.save(fp="C:\\Users\\minas\\Desktop\\giphy_1.gif",
+    output_gif.save(fp=os.path.join(settings.STATIC_DIR, (text+".gif")),
                     format='gif', save_all=True, append_images=frames[1:])
