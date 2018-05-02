18005c4871435596ec60e7223e176e2d05993cbd
Ian Minara <minashcaster@gmvsdvdsvail.com>
2018-05-03 01:01:53 +0300

    Complete overhaul

--- processcomm/settings.py
+++ processcomm/settings.py
@@ -16,7 +16,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
-
+FONTS_DIR = os.path.join(STATIC_DIR, 'fonts')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
